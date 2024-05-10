ea7c0da977e83385b3661b42827de87707d096cc
Victor I. Afolabi <javafolabi@gmail.com>
2024-05-10 16:22:02 +0100

    Update ai_news API

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
-import os
-from newsapi import NewsApiClient
-from dotenv import load_dotenv
 from pprint import pprint
-
-
-load_dotenv()
+from ai_news.news import Category, News
 
 
 def main() -> None:
-    newsapi = NewsApiClient(api_key=os.environ['NEWS_API_KEY'])
+    """Starting point."""
 
-    top_headlines = newsapi.get_top_headlines(
-        q='ai',
-        sources='bbc-news,the-verge',
-        language='en',
+    news = News()
+    category = Category.TECHNOLOGY
+    country = None
+    language = 'en'
+
+    # Sources.
+    sources = news.get_sources(
+        country=country,
+        category=category,
+        language=language,
     )
-    pprint(top_headlines)   # {articles:[], status: ok, totalResult: 0}
+    pprint(sources)
+    print(
+        f'There are {len(sources)} sources given {category=}, {country=}, {language=}'
+    )
 
     print(f'\n{"--" * 50}\n')
 
-    # sources = newsapi.get_sources()
-    # pprint(sources.keys())
+    # Top headlines.
+    headlines = news.get_top_headlines(
+        sources=None,
+    )
+    pprint(headlines[:3])
+    print(f'There are {len(headlines)} headlines')
+
+    print(f'\n{"--" * 50}\n')
+
+    # Everything.
+    articles = news.get_articles(
+        q='artificial intelligence',
+        sources=sources,
+    )
+    pprint(articles[:3])
+    print(len(articles))
+
+    article = articles[0]
+    print(f'{article.title = }')
+    print(f'{article.author = }')
+    print(f'{article.source = }')
+    print(f'{article.description = }')
+    print(f'{article.content = }')
+    print(f'{article.url = }')
+    print(f'{article.image_url = }')
+    print(f'{article.published_at = }')
 
 
 if __name__ == '__main__':
---
+++ src/ai_news/news/__init__.py
@@ -0,0 +1,10 @@
+from ai_news.news.news import News
+from ai_news.news.util import Category, Source, NewsArticle
+
+
+__all__ = [
+    'Category',
+    'News',
+    'NewsArticle',
+    'Source',
+]
